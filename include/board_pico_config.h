af33256b6cd491860fe3200a571ff50ef0fc7342
KunYi Chen <kunyi.chen@gmail.com>
2025-06-02 14:12:39 +0800

    enable nRESET for DEBUG ON PICO

--- include/board_pico_config.h
+++ include/board_pico_config.h
@@ -32,16 +32,14 @@
 // PIO config
 #define PROBE_SM 0
 #define PROBE_PIN_OFFSET 2
-#define PROBE_PIN_SWCLK (PROBE_PIN_OFFSET + 0) // 2
-#define PROBE_PIN_SWDIO (PROBE_PIN_OFFSET + 1) // 3
+#define PROBE_PIN_SWCLK (PROBE_PIN_OFFSET + 0) // GPIO2
+#define PROBE_PIN_SWDIO (PROBE_PIN_OFFSET + 1) // GPIO3
 // Target reset config
-#if false
-#define PROBE_PIN_RESET 1
-#endif
+#define PROBE_PIN_RESET 1 // GPIO1
 
 // UART config
-#define PROBE_UART_TX 4
-#define PROBE_UART_RX 5
+#define PROBE_UART_TX 4  // GPIO4
+#define PROBE_UART_RX 5  // GPIO5
 #define PROBE_UART_INTERFACE uart1
 #define PROBE_UART_BAUDRATE 115200
 
